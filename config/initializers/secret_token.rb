f2045ca5b07e9644cbc4af063f160705500a81df
little.129@osu.edu <little.129@osu.edu>
2013-03-03 15:31:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '18b366c0eb3d146401da485ac7f2a0f91aa0600fbd2f9c6fc07fd448fccee7b5ceffe33e317164b045f473ac314124982ff486cf42e740a90151b37f3da464e3'
